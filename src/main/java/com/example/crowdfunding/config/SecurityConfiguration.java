11ec3bfa34ac188f04085a9f2abf013462e70e0c
Fifolu Akinyemi <fkinyemi@gmail.com>
2022-03-10 01:05:10 +0000

    Exclude login endpoint from spring security

--- src/main/java/com/example/crowdfunding/config/SecurityConfiguration.java
+++ src/main/java/com/example/crowdfunding/config/SecurityConfiguration.java
@@ -44,6 +44,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/api/v1.0/users/newuser");
+        web.ignoring().antMatchers("/api/v1.0/users/login");
         web.ignoring().antMatchers("/api/v1.0/users/{id}");
     }
 
