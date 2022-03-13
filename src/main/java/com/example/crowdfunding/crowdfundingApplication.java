ee7b373a71f153844a76873d423a63ddbe24ebc8
Fifolu Akinyemi <fkinyemi@gmail.com>
2022-03-13 21:34:02 +0000

    Added cloudinary dependency and bean

--- src/main/java/com/example/crowdfunding/crowdfundingApplication.java
+++ src/main/java/com/example/crowdfunding/crowdfundingApplication.java
@@ -1,12 +1,27 @@
 package com.example.crowdfunding;
 
+import com.cloudinary.Cloudinary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication()
 public class crowdfundingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(crowdfundingApplication.class, args);
+	}
+
+	@Bean
+	public Cloudinary cloudinaryConfig() {
+		Map config = new HashMap();
+		config.put("cloud_name", System.getenv("cloud_name"));
+		config.put("api_key", System.getenv("api_key"));
+		config.put("api_secret", System.getenv("api_secret"));
+		Cloudinary cloudinary = new Cloudinary(config);
+		return cloudinary;
 	}
 }
